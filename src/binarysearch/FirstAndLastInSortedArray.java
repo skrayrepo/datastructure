8ace3450ac908f996c80dd46815edbb880abb7cf
Santosh <100818767+skrayrepo@users.noreply.github.com>
2022-11-16 17:49:08 +0530

    Added new files for data structure

--- src/binarysearch/FirstAndLastInSortedArray.java
+++ src/binarysearch/FirstAndLastInSortedArray.java
@@ -1,19 +1,25 @@
 package binarysearch;
 
+import java.util.Arrays;
+
 public class FirstAndLastInSortedArray {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{5,7,7,8,8,10};
+        int[] arr = new int[]{5,7,7,8,8,8,8,10};
         int target = 8;
+        int[] result = new int[]{-1,-1};
         //Find first and last index of the target number
-        String result= SearchBinary(arr,target);
-        System.out.println(result);
+        int startPosition= SearchBinary(arr,target,true);
+        int endPosition= SearchBinary(arr,target,false);
+        result[0] = startPosition;
+        result[1] = endPosition;
+        System.out.println(Arrays.toString(result));
     }
 
-    public static String SearchBinary(int[] arr, int target){
+    public static int SearchBinary(int[] arr, int target,boolean startPosition){
         int start = 0;
         int end = arr.length -1;
-        String result = "";
+        int result = -1;
         while(start <= end){
             int mid = (start+end)/2;
             if (target > arr[mid]) {
@@ -21,10 +27,11 @@ public class FirstAndLastInSortedArray {
             }else if(target < arr[mid]){
                 end = mid - 1;
             }else{
-                if (arr[mid] == arr[mid+1]) {
-                    return mid+","+mid+1;
-                }else if(arr[mid] == arr[mid-1]){
-                    return  mid-1+","+mid;
+                result = mid;
+                if (startPosition) {
+                    end = mid - 1;
+                }else {
+                    start = mid + 1;
                 }
             }
         }
