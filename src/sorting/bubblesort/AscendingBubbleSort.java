fa6c2b533ac80760d329bc3a90f99ed74759ffff
Santosh <100818767+skrayrepo@users.noreply.github.com>
2022-12-15 21:09:53 +0530

    Added new files for data structure

--- src/sorting/bubblesort/AscendingBubbleSort.java
+++ src/sorting/bubblesort/AscendingBubbleSort.java
@@ -1,26 +1,27 @@
-package bubblesort;
+package sorting.bubblesort;
 
 import java.util.Arrays;
 
-public class DescendingBubbleSort {
+public class AscendingBubbleSort {
     public static void main(String[] args) {
         int[] arr = {3,2,1,7,6,5,8,4};
-        descBubbleSort(arr);
-        System.out.println("Descending arry is"+ Arrays.toString(arr));
+        ascBubbleSort(arr);
+        System.out.println("Sorted array "+ Arrays.toString(arr));
     }
 
-    private static void descBubbleSort(int[] arr) {
+    private static void ascBubbleSort(int[] arr) {
         boolean alreadySorted = false;
-        for(int i = 0;i < arr.length;i++){
-            alreadySorted = true;
-            for(int j = 1;j < arr.length - i;j++){
-                if(arr[j] > arr[j-1]){
+        for(int i=0; i < arr.length;i++){
+            for(int j=1; j < arr.length - i; j++){
+                alreadySorted = true;
+                if(arr[j] < arr[j-1]){
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
                     alreadySorted = false;
                 }
             }
+            //
             if(alreadySorted){
                 break;
             }
