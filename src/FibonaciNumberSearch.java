15209362250e448456383de60adee2fe8565769f
Santosh <100818767+skrayrepo@users.noreply.github.com>
2022-12-22 20:51:13 +0530

    Added new files for data structure

--- src/FibonaciNumberSearch.java
+++ src/FibonaciNumberSearch.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class FibonaciNumberSearch {
     public static void main(String[] args) {
         int nth = 8;
         int[] arr = new int[nth];
-        for(int i = 0; i < nth; i++){
+        /*for(int i = 0; i < nth; i++){
             if(i == 0)
                 arr[i] = i;
             else if( i == 1)
@@ -11,10 +13,21 @@ public class FibonaciNumberSearch {
                 arr[i] = arr[i-1] + arr[i-2];
             }
 
+        }*/
+        int i = 0;
+        while(i< nth){
+            if(i==0)
+                arr[i] = i;
+            else if(i ==1)
+                arr[i] = i;
+            else
+                arr[i] = arr[i-1]+arr[i-2];
+            i++;
         }
-        for(int i = 0; i < nth; i++){
+        System.out.println("Fibonacii number will be"+ Arrays.toString(arr));
+        /*for(int i = 0; i < nth; i++){
             System.out.println("Fibonacci number series::" +arr[i]);
-        }
+        }*/
     }
 }
 
